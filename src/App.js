5b1b3bf7361035fde1b309fbff4c8739b6f72914
manojmaxxma <manojakshan421@gmail.com>
2020-05-25 16:52:08 +0530

    basics of the game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
 import React from 'react';
+import './gameStyles.css'
+class Square extends React.Component {
+    render() {
+        return (
+            <button
+                className="square">
+                {/*
+          todo
+          */}
+            </button>
+        )
+    }
+}
 
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square/>
+    }
 
-function App() {
-  return (
-    <div className="App">
+    render() {
+        const status = "next Player X"
+        return (
 
-    </div>
-  );
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        )
+    }
+
 }
 
 export default App;
